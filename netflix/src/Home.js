eb8258c971982ca8dd85b21dbe84a6042f08d811
jesu03 <95433671+jesu03@users.noreply.github.com>
2023-07-26 17:38:33 +0530

    create a login page

---
+++ netflix/src/Home.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Home.css'
+import {useNavigate} from 'react-router-dom';
+const Home = () => {
+  const res =  useNavigate();
+  const handleClick = () =>{
+    res("/home");
+  }
+  return (
+    <div className='home'>
+    <div className='home__header'>
+    <div className='home__header1'>
+    <img src="https://about.netflix.com/images/logo.png" alt="logo" />
+    <select className='home__drop'>
+        <option>English</option>
+        <option>Tamil</option>
+    </select>
+    <button className='home__button'>Sign in</button>
+    </div>
+    <div className='home__content'>
+    <h1>Unlimited movies, TV shows and more</h1>
+    <h3>Watch anywhere. Cancel anytime.</h3>
+    <h4>Ready to watch? Enter your email to create or restart your membership.</h4>
+    <input placeholder='Email Address'/>
+    <button className='home__button1' onClick={handleClick}>Get Started > </button>
+    </div>
+    </div>  
+    </div>
+  )
+}
+
+export default Home
